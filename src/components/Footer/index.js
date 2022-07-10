243dcc209ea897979f7906e7bc5bd04dd23d2845
Luki Nguyen <nguyenhien236259@gmail.com>
2022-07-10 19:48:31 +0700

    test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,28 +7,121 @@ function Footer({
   listColumn,
   styles
 }) {
+  const stylesInline = {
+    footer: {
+      backgroundColor: '#24262b',
+      padding: '70px 0'
+    },
+    container: {
+      maxWdth: '1170px',
+      margin: 'auto'
+    },
+    row: {
+      display: 'grid',
+      flexWrap: 'wrap',
+      gridTemplateColumns: 'auto auto auto auto',
+      gap: '10px'
+    },
+    footerCol_ul: { listStyle: 'none', padding: '0', textAlign: 'left' },
+    footerCol: { width: '100%', padding: '0 15px' },
+    footerCol_h4: {
+      fontSize: '18px',
+      textTransform: 'capitalize',
+      marginBottom: '35px',
+      fontWeight: '500',
+      position: 'relative',
+      textAlign: 'left',
+      minHeight: '20px'
+    },
+    footerCol_h4_before: {
+      '&::before': {
+        content: "''",
+        position: 'absolute',
+        left: '0',
+        bottom: '-10px',
+        backgroundColor: '#f5941a',
+        height: '2px',
+        boxSizing: 'border-box',
+        width: '50px'
+      }
+    },
+    footerCol_ul_li_not__last_child: {
+      '&::not(:last-child)': {
+        marginBottom: '10px'
+      }
+    },
+    footerCol_ul_li_a: {
+      fontSize: '16px',
+      textTransform: 'capitalize',
+      textDecoration: 'none',
+      fontWeight: '300',
+      transition: 'all 0.3s ease',
+      display: 'flex',
+      gap: '10px',
+      '&::hover': {
+        color: '#ffffff',
+        paddingLeft: '8px'
+      }
+    },
+    footerCol__socialLinks_a: {
+      display: 'inline-block',
+      height: '40px',
+      width: '40px',
+      backgroundColor: 'rgba(255,255,255,0.2)',
+      margin: '0 10px 10px 0',
+      textAlign: 'center',
+      lineHeight: '40px',
+      borderRadius: '50%',
+      color: '#ffffff',
+      transition: 'all 0.5s ease',
+      '&::hover': {
+        color: '#24262b',
+        backgroundColor: '#ffffff'
+      }
+    },
+    '@media (max-width: 767px)': {
+      __expression__: '(max-width: 767px)',
+      footerCol: { width: '50%', marginBottom: '30px' }
+    },
+    '@media (max-width: 574px)': {
+      __expression__: '(max-width: 574px)',
+      footerCol: { width: '100%' }
+    }
+  }
   return (
     <footer
-      className={styles.footer}
+      className={stylesInline.footer}
       style={{ background: background || '#24262b' }}
     >
-      <div className={styles.container}>
-        <div className={styles.row}>
+      <div className={stylesInline.container}>
+        <div className={stylesInline.row}>
           {listColumn.map((e, i) => {
             return (
-              <div className={styles.footerCol} key={i}>
+              <div className={stylesInline.footerCol} key={i}>
                 <h4
-                  className={e.title !== '' ? styles.before : styles.empty}
+                  className={[
+                    stylesInline.footerCol_h4,
+                    e.title !== ''
+                      ? stylesInline.footerCol_h4_before
+                      : stylesInline.empty
+                  ]}
                   style={{ color: colorTitle || '#ffffff' }}
                 >
                   {e.title}
                 </h4>
-                <ul>
+                <ul className={stylesInline.footerCol_ul}>
                   {e.options.map((c, index) => {
                     return (
-                      <li key={index}>
+                      <li
+                        key={index}
+                        className={
+                          index !== c.option &&
+                          stylesInline.footerCol_ul_li_not__last_child
+                        }
+                      >
                         {c.blank ? (
                           <a
+                            className={stylesInline.footerCol_ul_li_a}
                             href={c.link}
                             target='_blank'
                             rel='noopener noreferrer'
@@ -39,6 +132,7 @@ function Footer({
                           </a>
                         ) : (
                           <a
+                            className={stylesInline.footerCol_ul_li_a}
                             href={c.link}
                             style={{ color: colorLabel || '#bbbbbb' }}
                           >
