9fdef23efd0ac14c11b42f6c2ad38fca1cde6c33
Luki Nguyen <nguyenhien236259@gmail.com>
2022-07-10 17:28:03 +0700

    'test'

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { FooterFast } from '.'
 
-describe('ExampleComponent', () => {
+describe('FooterFast', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(FooterFast).toBeTruthy()
   })
 })
